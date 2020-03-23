71eb845f47eef25317eed8586a1582a0075cd860
jaffamonkey <paullittlebury@mail.com>
2020-03-23 14:10:16 +0000

    tests update

--- python3-selenium-headless.py
+++ python3-selenium-headless.py
@@ -13,9 +13,12 @@ chrome_driver = os.getcwd() + os.sep + "chromedriver"
 
 # go to Google and click the I'm Feeling Lucky button
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-driver.get("https://www.google.com")
-lucky_button = driver.find_element_by_css_selector("[name=btnI]")
-lucky_button.click()
-
-# capture the screen
-driver.get_screenshot_as_file("capture.png")
+driver.get("https://duckduckgo.com")
+        searchField = driver.find_element_by_name("q")
+        searchField.send_keys("TrumpKlon")
+        searchButton = driver.find_element_by_css_selector("[name=btnI]")
+        searchButton.click()
+        self.assertIn("TrumpKlon", driver.title)
+        assert "Donald Trump personality simulator in the world." in driver.page_source
+        # capture the screen
+driver.get_screenshot_as_file("headless_test_end_capture.png")
--- python3-selenium.py
+++ python3-selenium.py
@@ -5,17 +5,18 @@ from selenium.webdriver.common.keys import Keys
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
 
     def test_search_in_python_org(self):
         driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
-
+        driver.get("https://duckduckgo.com")
+        searchField = driver.find_element_by_name("q")
+        searchField.send_keys("TrumpKlon")
+        searchButton = driver.find_element_by_css_selector("[name=btnI]")
+        searchButton.click()
+        self.assertIn("TrumpKlon", driver.title)
+        assert "Donald Trump personality simulator in the world." in driver.page_source
+        driver.get_screenshot_as_file("test_end_capture.png")
 
     def tearDown(self):
         self.driver.close()
