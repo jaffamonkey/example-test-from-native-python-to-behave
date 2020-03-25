547302309dafdf56923718207ecd66dfa69eccfc
jaffamonkey <paullittlebury@mail.com>
2020-03-25 02:58:35 +0000

    updates to chromedriver

--- python3-selenium.py
+++ python3-selenium.py
@@ -2,15 +2,14 @@ import unittest
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 
 class PythonOrgSearch(unittest.TestCase):
 
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=os.getcwd() + os.sep + "chromedriver")
+
     def test_search_in_python_org(self):
-        chrome_options = Options()
-        chrome_options.add_argument("--window-size=1920x1080")
-        chrome_driver = os.getcwd() + os.sep + "chromedriver"
-        driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
+        driver = self.driver
         driver.get("https://duckduckgo.com")
         searchField = driver.find_element_by_name("q")
         searchField.send_keys("TrumpKlon")
@@ -19,6 +18,9 @@ class PythonOrgSearch(unittest.TestCase):
         self.assertIn("TrumpKlon", driver.title)
         assert "Donald Trump personality simulator in the world." in driver.page_source
         driver.get_screenshot_as_file("test_end_capture.png")
+
+    def tearDown(self):
+        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
