3f398254ad42431b4799007c525bbcbb3645cc17
jaffamonkey <paullittlebury@mail.com>
2020-03-25 00:08:57 +0000

    updates to chromedriver

--- features/environment.py
+++ features/environment.py
@@ -5,10 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
-    options = Options()
-    options.headless = True
-    CHROMEDRIVER_PATH = os.getcwd() + "/bin/chromedriver"
-    context.driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
+    chrome_options = Options()
+    chrome_options.headless = True
+    chrome_path = os.getcwd() + "/bin/chromedriver"
+    context.driver = webdriver.Chrome(options=chrome_options)
     context.driver = webdriver.Chrome()
     context.driver.implicitly_wait(10)
     parser = configparser.ConfigParser()
